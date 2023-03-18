e30dc82ff3a1af99ad31b6c222609443e4a504ff
Atamatov Oybek <oybekatamatov999@gmail.com>
2023-03-18 20:55:08 +0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint, choice
 numbers = [str(i) for i in range(10)]
 big_alpha = [chr(i) for i in range(65, 91)]
-low_alpha = [chr(i) for i in range(141, 173)]
+low_alpha = list(map(lambda x: x.lower(), big_alpha))
 symbol = list('@#$?!&')
 
 sign = {1:numbers , 2:big_alpha, 3:low_alpha, 4:symbol}
@@ -10,5 +10,5 @@ password = ''
 length = randint(8,15)
 for i in range(length):
     password += choice(sign[randint(1,4)])
-    
+
 print(password)
