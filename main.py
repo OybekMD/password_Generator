09bf2c426f0dadae7a449b427eff0df0d6282bc6
Atamatov Oybek <oybekatamatov999@gmail.com>
2023-03-18 20:09:50 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from random import randint, choice
+numbers = [str(i) for i in range(10)]
+big_alpha = [chr(i) for i in range(65, 91)]
+low_alpha = [chr(i) for i in range(141, 173)]
+symbol = list('@#$?!&')
+
+sign = {1:numbers , 2:big_alpha, 3:low_alpha, 4:symbol}
