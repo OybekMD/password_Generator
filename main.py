b6aeee89b358491d4ed231f2c5e1b0ebdc7f24cd
Atamatov Oybek <oybekatamatov999@gmail.com>
2023-03-18 20:20:51 +0500

    modified:   main.py

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ low_alpha = [chr(i) for i in range(141, 173)]
 symbol = list('@#$?!&')
 
 sign = {1:numbers , 2:big_alpha, 3:low_alpha, 4:symbol}
+
+password = ''
+length = randint(8,15)
+for i in range(length):
+    password += choice(sign[randint(1,4)])
+    
+print(password)
